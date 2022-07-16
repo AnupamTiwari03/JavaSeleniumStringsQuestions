213c9dfa03645fa183eb1948366e2115848ce8c0
Anupam Tiwari <tiwarianupam02021@gmail.com>
2022-07-16 21:01:17 +0530

    New Program to find no  of character in a given string

---
+++ JavaSeleniumStringQuestions/src/com/javaseleniumstringproblems/FindCharacterInString.java
@@ -0,0 +1,22 @@
+package com.javaseleniumstringproblems;
+
+public class FindCharacterInString {
+
+	public static void main(String[] args) {
+		// find how many character are present inside string
+
+		String data = "Automation Tester";
+		int count = 0;
+
+		for (int i = 0; i < data.length(); i++) {
+			if (data.charAt(i) != ' ') {
+				count = count + 1;
+
+			}
+
+		}
+		System.out.println("No of Character in a given string is : " + count);
+
+	}
+
+}
